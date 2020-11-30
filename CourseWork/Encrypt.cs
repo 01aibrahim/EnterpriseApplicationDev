23422187b55b3bce1e9ee07d06d0a65ab656a0f9
Abdulrahman Ibrahim <51540733+01aibrahim@users.noreply.github.com>
2020-11-30 19:58:02 +0000

    Add files via upload

---
+++ CourseWork/Encrypt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace wpfAibrahimW1680174
+{
+    public static class Encrypt
+    {
+
+
+
+
+
+    }
+}
